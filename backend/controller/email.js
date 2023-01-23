18b130b73338076fad8b85fd3ba86af0ebb93b19
luckyzluz <2567046155@qq.com>
2023-01-23 19:46:37 +0800

    Improve the mailbox verification module

--- backend/controller/email.js
+++ backend/controller/email.js
@@ -1,9 +1,10 @@
 const MysqlMethods = require('../util/mysql')
 const moment = require('moment')
 const { DateSort, DateSortx, getTimeInfo } = require('../util/utils')
-const nodemail =require('../util/nodemailer')
-const {redisDb} = require('../util/redis');
-const {EmailVerifyConfig} =require('../config/config.default');
+const nodemail = require('../util/nodemailer')
+const { redisDb } = require('../util/redis');
+const { EmailVerifyConfig } = require('../config/config.default');
+const { REDIS_CONFIG } = require("../config/config.db")
 // let isEmailVerify=false;// 是否启用邮箱验证码
 
 // 生成的随机六位数
@@ -18,54 +19,76 @@ function createSixNum() {
 exports.email = async (req, res, next) => {
     try {
         if(EmailVerifyConfig.isEmailVerify){ // 是否启用邮箱验证码
-            var user =req.body.user;
+            var user = req.body.user;
             // delete user.password;
-            var code = await createSixNum(); // 这里是我写的生成的随机六位数，等等下面给代码
-            user.code=code;
+            var code = createSixNum(); // 这里是我写的生成的随机六位数，等等下面给代码
+            user.code = code;
             // email= user.email
             // time = new Date().getTime()
-            let status=0; // 当前状态 
+            let status = 0; // 当前状态 
     
-            // status  0：初始状态  1：成功 2：邮件发送失败 3:验证码缓存失败
-    
-            // 发送验证邮件
-        await nodemail(user.email,code).then(res=>{
-            if(res.response.indexOf('OK')>=0){ // 发送成功
-                status=1;
-            }else{ // 发送失败
-                status=2;
-            }
-        })
+            // status  0：发送失败  1：成功 2：缓存失败 3:已发送
 
-        await redisDb.set('0',user.email,code,5*60).then(res=>{
-            res=='OK'?status=1:status=3;
-        });
-        if(status==1){
-            res.status(200).json({
-                code:20000,
-                // yztxt:code,
-                message:"验证码发送成功"
+            // 查询是否已发放验证码
+            await redisDb.get(REDIS_CONFIG.database._user, user.email).then(res => {
+                res == null ? '' : status = 3;
             })
-        }else if(status==2){
-            res.status(200).json({
-                code:50000,
-                message:"验证码发送失败，请检查邮箱配置"
-            })
-        }else if(status==3){
-            res.status(200).json({
-                code:50000,
-                message:"验证码发送失败,请查看后台日志"
-            })
-        }else{
-            res.status(200).json({
-                code:50000,
-                message:"验证码发送失败"
-            })
-        }
+            console.log(status)
+            if(status !== 3){
+                // 发送验证邮件
+                await nodemail(user.email,code).then(res => {
+                    if(res.response.indexOf('OK') >= 0){ // 发送成功
+                        status = 1;
+                    }else{ // 发送失败
+                        status = 0;
+                    }
+                })
+            }
+
+            if(status == 1){
+                await redisDb.set(REDIS_CONFIG.database._user, user.email, code, 5*60).then(res=>{
+                    res == 'OK' ? status = 1 : status = 2;
+                });
+            }
+            
+            if(status == 3){
+                res.status(200).json({
+                    code: 40000,
+                    success: false,
+                    // yztxt:code,
+                    message:"验证码已发送"
+                })
+            }else if(status == 1){
+                res.status(200).json({
+                    code: 20000,
+                    success: true,
+                    // yztxt:code,
+                    message:"验证码发送成功"
+                })
+            }else if(status==0){
+                res.status(200).json({
+                    code:50000,
+                    success: false,
+                    message:"验证码发送失败，请检查邮箱配置"
+                })
+            }else if(status == 2){
+                res.status(200).json({
+                    code:50000,
+                    success: false,
+                    message:"验证码发送失败,请查看后台日志"
+                })
+            }else{
+                res.status(200).json({
+                    code: 50000,
+                    success: false,
+                    message: "验证码发送失败"
+                })
+            }
         }else{
             res.status(200).json({
                 code:40001,
-                msg:"邮箱验证未开启"
+                success: false,
+                message:"邮箱验证未开启"
             })
         }
         
--- backend/validator/email.js
+++ backend/validator/email.js
@@ -8,6 +8,8 @@ const { redisConfig } = require('../config/config.default')
 const {redisDb} = require('../util/redis');
 // const {User} = require('../model')
 const md5 = require('../util/md5')
+const { REDIS_CONFIG, mysqlUserKey } = require("../config/config.db")
+
 exports.email = validate([
     body('user.email')
     .notEmpty().withMessage('邮箱不能为空')
@@ -17,59 +19,66 @@ exports.email = validate([
     .custom(async email => {
         let isGo =true; // 是否继续mysql查询
         let emailStatus=0; // 查询状态 0：不存在  1：存在 2：不存在但验证码已发送
+        let user = [];
 
-        if(redisConfig.isRedis){ // 是否开启了redis缓存
-            await redisDb.hGet(0,'Email.to.id',email).then(res=>{
-            res==null?isGo=true:isGo=false;
-            res==null?emailStatus=0:emailStatus=1;
-            })
-        }
+        await redisDb.hGet(REDIS_CONFIG.database._user, 'Email.to.id', email).then(res => {
+            if(res == null){
+                isGo = true;
+                emailStatus = 0;
+            }
+            res == null ? isGo = true : isGo = false;
+            res == null ? emailStatus = 0 : emailStatus = 1;
+        })
 
-        if(isGo){
-            const isEmail = await Knex.select('user_id').where({user_email:email}).from("lz_users");
-            // await MysqlMethod.select('*','lz_users',`where user_email="${email}"`);
-            // await Knex.select({fileld:"*",options:{user_email:email}},{name:'user_regtime',order:'desc'})
-            isEmail.length>0?emailStatus=1:emailStatus=0;
-            
+        if(isGo){ // 继续mysql查询
+            let mysqlSelectParams = {
+                field: '*',
+                options: {}
+            }
+            mysqlSelectParams.options[mysqlUserKey.email] = email;
+
+            user = await User.select(mysqlSelectParams);
+            user.length > 0 ? emailStatus = 1 : emailStatus = 0;
         }
-        if(emailStatus==0){
-            await redisDb.get("0",email).then(res=>{
-                res==null?'':emailStatus=2;
-            })
-        }
-        // const user = await User.findOne({email})
+        
+        // if(emailStatus == 0){
+        //     await redisDb.get(REDIS_CONFIG.database._user, email).then(res => {
+        //         res == null ? '' : emailStatus = 2;
+        //     })
+        // }
         // console.log(emailStatus)
-        if(emailStatus==1){ // 邮箱存在则进行redis缓存
-            return Promise.reject('邮箱已存在')
-        }else if(emailStatus==2){
-            return Promise.reject('验证码已发送')
+        if(emailStatus == 1){ // 邮箱存在则进行redis缓存
+            return Promise.reject('该邮箱账号已存在')
         }
+        // else if(emailStatus == 2){
+        //     return Promise.reject('验证码已发送')
+        // }
     }),
     // body('user.password').notEmpty().withMessage('密码不能为空'),
-    body('user.username')
-    .notEmpty().withMessage('用户名不能为空')
-    .bail()
-    .custom(async username => {
-        let isGo =true; // 是否继续mysql查询
-        let nameResult=false; // 用户名查询结果
+    // body('user.username')
+    // .notEmpty().withMessage('用户名不能为空')
+    // .bail()
+    // .custom(async username => {
+        // let isGo =true; // 是否继续mysql查询
+        // let nameResult=false; // 用户名查询结果
 
-        if(redisConfig.isRedis){ // 是否开启了redis缓存
-        // 查询redis是否存在用户名(存在则跳过mysql查询)
-        await redisDb.hGet(0,'Username.to.id',username).then(res=>{
-            res==null?isGo=true:isGo=false;
-            res==null?nameResult=false:nameResult=true;
-        })
-        }
+        // if(redisConfig.isRedis){ // 是否开启了redis缓存
+        // // 查询redis是否存在用户名(存在则跳过mysql查询)
+        // await redisDb.hGet(0,'Username.to.id',username).then(res=>{
+        //     res==null?isGo=true:isGo=false;
+        //     res==null?nameResult=false:nameResult=true;
+        // })
+        // }
 
-        if(isGo){
-        // 查询mysql是否存在用户名
-        const isUsername = await Knex.select('user_id').where({user_name:username}).from("lz_users");
-        // await MysqlMethod.select('*','lz_users',`where user_name="${username}"`);
-        isUsername.length>0?nameResult=true:nameResult=false;
-        }
-        // const user = await User.findOne({username})
-        if(nameResult){
-            return Promise.reject('用户名已存在')
-        }
-    })
+        // if(isGo){
+        // // 查询mysql是否存在用户名
+        // const isUsername = await Knex.select('user_id').where({user_name:username}).from("lz_users");
+        // // await MysqlMethod.select('*','lz_users',`where user_name="${username}"`);
+        // isUsername.length>0?nameResult=true:nameResult=false;
+        // }
+        // // const user = await User.findOne({username})
+        // if(nameResult){
+        //     return Promise.reject('用户名已存在')
+        // }
+    // })
 ])
