197ab63bc44db7fa222c475ac31f6ffe01f4995c
luckyzluz <2567046155@qq.com>
2023-01-24 21:02:38 +0800

    Improve mailbox configuration

---
+++ backend/config/config.email.js
@@ -0,0 +1,20 @@
+/**
+ * 邮箱配置
+ */
+
+module.exports = {
+    EmailVerifyConfig:{
+        isEmailVerify: false,
+        smtpConfig: {
+          host: 'smtp.qq.com',
+          port: 465,
+          secure: true, // 使用 SSL
+          secureConnection: true, // 使用 SSL
+          auth: {
+            user: '2567046155@qq.com', //注册的邮箱账号
+            pass: 'eckhfvvstjendjgh' //邮箱的授权码，不是注册时的密码,等你开启的stmp服务自然就会知道了
+          }
+        },
+        Template: 'letter'
+      }
+}
