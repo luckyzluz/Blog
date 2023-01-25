3f9f80f012b020eb31a00db584f5eafc199105ef
luckyzluz <2567046155@qq.com>
2023-01-25 18:12:48 +0800

    Optimize mysql database operation module

--- backend/model/base.js
+++ backend/model/base.js
@@ -1,14 +1,24 @@
 const knex = require('../model/knex');
-const {mysqlUserKey} = require("../config/config.db")
+const { mysqlUserKey, mysqlArtKey } = require("../config/config.db")
 
 class Base{
   constructor(props){
     this.table = props;
+    if(props == 'lz_users'){
+      this.ordername = mysqlUserKey.regtime;
+      this.id = mysqlUserKey.id;
+    }else if(props == 'lz_article'){
+      this.ordername = mysqlArtKey.regtime;
+      this.id = mysqlArtKey.id;
+    }
   }
 
   // 查找
-  all (){
-    return knex(this.table).select();
+  all (order, offset, limit){
+    let orders= order ? order : 'desc';
+    let pages= offset ? offset - 1 : 0;
+    let pieces= limit ? limit : 10;
+    return knex(this.table).select().orderBy(this.ordername, orders).limit(pieces).offset(pages);
   }
 
   /**
@@ -17,17 +27,16 @@ class Base{
    * @param {*} orders 顺序
    * @returns 
    */
-  select (params,orders){
+  select (params, order, offset, limit){
     // let result;
-    let orderx=orders?orders:{
-      name:'user_regtime',
-      order:'desc'
-    }
+    let orders= order ? order : 'desc'
+    let pages= offset ? offset - 1 : 0;
+    let pieces= limit ? limit : 10;
     let options=params.options;
     if(options){
-      return knex.select(params.field).from(this.table).where(options).orderBy(orderx.name, orderx.order)
+      return knex.select(params.field).from(this.table).where(options).orderBy(this.ordername, orders).limit(pieces).offset(pages);
     }else{
-      return knex.select(params.field).from(this.table).orderBy(orderx.name, orderx.order)
+      return knex.select(params.field).from(this.table).orderBy(this.ordername, orders).limit(pieces).offset(pages);
     }
     // return result;
     // knex.column('id').select().from('lz_member')
@@ -43,7 +52,7 @@ class Base{
 
   // 更改
   update (id, params){
-    return knex(this.table).where('user_id', '=', id).update(params);
+    return knex(this.table).where(this.id, '=', id).update(params);
   }
 
   /**
@@ -52,7 +61,7 @@ class Base{
    * @returns Number 0: 不存在该条数据  1以及以上代表删除条数
    */
   delete (id){
-    return knex(this.table).where('user_id', '=', id).del();
+    return knex(this.table).where(this.id, '=', id).del();
   }
 
 }
