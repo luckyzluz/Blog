52f6201af53dbe9349749973bf6b58cee14a87f8
luckyzluz <2567046155@qq.com>
2023-02-11 16:51:51 +0800

    Article parameter validation module optimization

--- backend/validator/article.js
+++ backend/validator/article.js
@@ -6,7 +6,7 @@ const validate = require('../middleware/validator')
 const MysqlMethods = require('../util/mysql')
 const ArtRedis =require("../util/getArticle")
 const Knex = require('../model/knex')
-const { mysqlArtKey } = require('../config/config.db')
+const { mysqlArtKey, mysqlUserKey } = require('../config/config.db')
 const { QueryArtInfos } = require('../util/Article');
 // const mongoose = require('mongoose')
 // const { Article } = require('../model')
@@ -14,126 +14,125 @@ const { QueryArtInfos } = require('../util/Article');
 //获取文章列表验证数据check
 exports.getArticles = validate([
     // async (res,req,next)=>{
-        query('offset').custom(async (value,{req})=>{
-            // console.log(value > req.query.limit)
-            if(value <= 0){
-                return Promise.reject('不得小于且等于0')
-            }
-            if(Number(value) > Number(req.query.limit)){
+        query('offset').isInt({min: 1}).withMessage('offset无效值'),
+        query('limit').isInt({min: 4}).withMessage('limit无效值').bail().custom(async(value, {req}) => {
+            if(Number(value) < Number(req.query.offset)){
                 return Promise.reject('limit不得小于offset')
             }
         }),
-        query('limit').custom(async(value) => {
-            // console.log(typeof(value))
-            if(Number(value) < 4){
-                return Promise.reject('limit不得小于4')
-            }
-        }),
-        query('orderby').custom(async(value) => {
-            // console.log(value == "")
-
-            if(!(value == 'like' || value == 'last' || value == 'views' || value == 'comment_count' || value== undefined)){
-                // console.log(333)
-                return Promise.reject('The orderby parameter value is illegal');
-            }
-        }),
-        query('sort').custom(async(value) => {
-            if(!(value == 'desc' || value == 'asc' || value== undefined)){
-                // console.log(333)
-                return Promise.reject('The sort parameter value is illegal');
-            }
-        })
+        query('orderby').isIn(['like', 'last', 'views', 'comment_count', undefined]).withMessage("The orderby parameter value is illegal"),
+        query('sort').isIn(['desc', 'asc', undefined]).withMessage("The sort parameter value is illegal")
     // }
     
 ])
 // 创建文章时验证数据
 exports.createArticle = validate([
     body('article.title').notEmpty().withMessage('文章标题不能为空'),
+    body('article.en').if(body('article.en').exists()).isLocale().withMessage("en需小写"),
+    body('article.letter').if(body('article.letter').exists()).isByteLength({max:1}).withMessage('文章首字母不符规范').bail().toUpperCase(),
+    body('article.status').if(body('article.status').exists()).isIn([1,0,undefined]).withMessage('status不符规范'),
     body('article.blurb').notEmpty().withMessage('文章摘要不能为空'),
     body('article.content').notEmpty().withMessage('文章内容不能为空'),
+    body('article.addtime').if(body('article.addtime').exists()).isLength({ min: 10,max:10 }).withMessage('addtime不符规范'),
+    body('article.time').if(body('article.time').exists()).isLength({ min: 10,max:10 }).withMessage('addtime不符规范'),
+    body('article.password').if(body('article.password').exists()).isLength({ min: 6 }).withMessage('password不能小于6位'),
+    body('article.type').notEmpty().withMessage('type不能为空').bail().isInt({min: 0}).withMessage('type不能小于0'),
+    body('article.type_1').notEmpty().withMessage('type_1不能为空').bail().isInt({min: 0}).withMessage('type_1不能小于0'),
+    body('article.comment_count').if(body('article.comment_count').exists()).isInt({min: 0}).withMessage('type_1不能小于0'),
+    body('article.level').if(body('article.level').exists()).isInt({min: 0, max: 9}).withMessage('level不符规范'),
+    body('article.istop').if(body('article.istop').exists()).isIn([0, 1]).withMessage('istop不符规范'),
+    body('article.lock').if(body('article.lock').exists()).isIn([0, 1]).withMessage('lock不符规范'),
+    body('article.up').if(body('article.up').exists()).isInt({min :0}).withMessage('up不符规范'),
+    body('article.down').if(body('article.down').exists()).isInt({min: 0}).withMessage('down不符规范'),
+    body('article.hits').if(body('article.hits').exists()).isInt({min :0}).withMessage('hits不符规范'),
+    body('article.hits_day').if(body('article.hits_day').exists()).isInt({min :0}).withMessage('hits_day不符规范'),
+    body('article.hits_week').if(body('article.hits_week').exists()).isInt({min :0}).withMessage('hits_week不符规范'),
+    body('article.hits_month').if(body('article.hits_month').exists()).isInt({min :0}).withMessage('hits_month不符规范'),
+    body('article.content').notEmpty().withMessage('文章内容不能为空'),body('article.content').notEmpty().withMessage('文章内容不能为空'),
+
 ])
 
 // 获取文章时验证数据(封装验证ID格式是否有效)
 exports.getArticle = validate([
-    param('articleId').custom(async value => {
-        // if(!mongoose.isValidObjectId(value)){
-        //     //返回一个失败状态的 Promise
-        //     return Promise.reject('文章ID类型错误')
-        //     //同步:失败
-        //     throw new Error('文章ID类型错误')
-        // }
-        // 同步:成功
-        return true
-    })
-    // 第二种方法
-    // validate.isValidObjectId(['params'],'articleId')
+    param('Id').isInt({min: 0}).withMessage('Id不符规范')
 ])
 
 // 更新文章时验证数据(封装验证ID是否有效)
 exports.updateArticle = [
     // 验证ID是否有效
-    // validate([
-    //     validate.isValidObjectId(['params'],'articleId')
-    // ]),
+    validate([
+        param('Id').isInt({min: 0}).withMessage('Id不符规范')
+    ]),
     // 验证文章是否存在
     async (req, res, next) => {
-        // const articleId = req.body.artId;
-        // await ArtRedis.getArtsList('0','artLists',articleId).then((res)=>{
-        //     console.log(res)
-        // })
-        // let art_result=await MysqlMethods.select('*','lz_article',`where id in (${req.body.artId.toString()})`)
-        // req.article = art_result
-        // console.log(req.params.articleId)
-
-        let selectResult = await QueryArtInfos([req.params.articleId]);
-        // await Knex(mysqlArtKey.table).where(mysqlArtKey.id, req.params.articleId).select();
+        let selectResult = await QueryArtInfos([req.params.Id]);
         // console.log(selectResult);  // [{}]
         if(JSON.stringify(selectResult[0]) == "{}"){
             return res.status(404).json({
                 code: 40004,
                 success: false,
-                message: '该文章数据不存在'
+                message: '该文章Id无效'
             });
+        }else{
+            req.article = selectResult[0];
         }
-        // if(!article){
-        //     return res.status(404).end()
-        // }
         next()
     },
     //验证所更新文章作者是否本人
-    // async (req, res, next) => {
-        // if(req.user._id.toString() !== req.article.author.toString()) {
-        //     return res.status(403).end()
-        // }
-        // console.log(req.user[0].zuser_id,req.article[0].art_author)
-        // if(req.user[0].zuser_id.toString() !== req.article[0].art_author.toString()) {
-            // return res.status(403).end()
-        // }
-        // next()
-    // }
+    async (req, res, next) => {
+        // console.log(req.user[mysqlUserKey.id],req.article[mysqlArtKey.author])
+        if(req.user[mysqlUserKey.id].toString() !== req.article[mysqlArtKey.author].toString()) {
+            return res.status(403).json({
+                code: 40003,
+                success: false,
+                message: "没有权限操作数据"
+            })
+        }
+        next()
+    }
 ]
-//校验文章是否存在
-// 修改的文章作者是否为当前用户
-
 // 删除文章
 exports.deleteArticle = [
-    //判断文章权限
+    // 验证ID是否有效
+    validate([
+        check('id.*').isInt({min: 0}).withMessage('Id不符规范')
+    ]),
+    // 验证文章是否存在(不用验证直接下一步)
+    // async (req, res, next) => {
+    //     // console.log(req.body.id)
+    //     let selectResult = await QueryArtInfos(req.body.id);
+    //     console.log(selectResult);  // [{}]
+    //     selectResult.forEach((v, i) => {
+    //         if(JSON.stringify(v) == "{}"){
+    //             return res.status(404).json({
+    //                 code: 40004,
+    //                 success: false,
+    //                 message: '该文章Id无效'
+    //             });
+    //         }else{
+    //             req.article = selectResult[0];
+    //         }
+    //     });
+        
+    //     next()
+    // },
+    //验证所更新文章作者是否本人
     async (req, res, next) => {
-        let isArtExists=0;
-        let dontdel=[];
-        // let artId=req.body.artId;
-        // let art_result=await MysqlMethods.select('id,member_id','lz_article',`where id in (${req.body.artId.toString()})`);
-        // if(art_result.length<=0){
-        //     return res.status(404).end("文章不存在");
-        // }
-        // for(let item in art_result){
-        //     if(art_result[item].member_id!==req.user[0].id){
-        //         dontdel.push(artId[item]);
-        //     }
-        // }
-        // if(dontdel.length>0){
-        //     return res.status(404).end("存在操作权限问题，请检查")
-        // }
+        let noPower = [];
+        let selectResult = await QueryArtInfos(req.body.id);
+        // console.log(selectResult);  // [{}]
+        selectResult.forEach((v, i) => {
+            if(req.user[mysqlUserKey.id].toString() !== v[mysqlArtKey.author]){
+                noPower.push(v[mysqlArtKey.id]);
+            }
+        });
+        if(noPower.length > 0) {
+            return res.status(403).json({
+                code: 40003,
+                success: false,
+                message: "没有权限批量操作数据"
+            })
+        }
         next()
-    },
+    }
 ]
