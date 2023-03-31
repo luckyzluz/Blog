75ffbd721af007073990fe7680aba98cde213f6b
luckyzluz <2567046155@qq.com>
2023-03-31 20:16:08 +0800

    New module

--- frontend/frontpage/src/views/home.vue
+++ frontend/frontpage/src/views/home.vue
@@ -6,12 +6,14 @@
     <Articlemini />
     <Aggregation />
     <HomeTabCon />
+    <!-- <Test /> -->
 </template>
 <script setup>
 import Slider from 'c/Slider.vue'
 import Articlemini from 'c/articlemini.vue'
 import Aggregation from 'c/aggregation.vue'
 import HomeTabCon from 'c/homeTabCon.vue'
+// import Test from '@/views/test.vue'
 import { reactive,watch, watchEffect,onMounted,getCurrentInstance } from 'vue'
 import { useStore } from "vuex";
 let {state,getters, dispatch,commit} = useStore();
