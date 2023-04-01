d0126b18c02cd534fb913aa5b004ec322df41ab0
luckyzluz <2567046155@qq.com>
2023-04-02 07:30:51 +0800

    funk

--- backend/config/config.email.js
+++ backend/config/config.email.js
@@ -15,7 +15,7 @@ module.exports = {
           secureConnection: true, // 使用 SSL
           auth: {
             user: '2567046155@qq.com', //注册的邮箱账号
-            pass: 'eckhfvvstjendjgh' //邮箱的授权码，不是注册时的密码,等你开启的stmp服务自然就会知道了
+            pass: '' //邮箱的授权码，不是注册时的密码,等你开启的stmp服务自然就会知道了
           }
         },
         EffectiveTime: 5*60,
