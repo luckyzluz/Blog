acb23a8c850c7f70f2322a6bdcfdfe1d7f6230a3
luckyzluz <2567046155@qq.com>
2023-04-03 20:57:27 +0800

    Test Airplayer player

--- frontend/frontpage/src/components/homeTabCon.vue
+++ frontend/frontpage/src/components/homeTabCon.vue
@@ -21,7 +21,7 @@
                             <a v-for="(x,y) in options">{{x.name}}</a>
                         </ul>
                     </div>
-                    <ArticleList v-for="(x,y) in tab.latestList.lists" :Data="{'data':x,'index':y}" />
+                    <ArticleList v-for="(x,y) in tab.latestList.lists" :listStyle="tab.latestList.styleType" :Data="{'data':x,'index':y}" />
                     <div class="theme-pagination">
                         <div class="next-page ajax-next">
                             <a href="">
@@ -32,7 +32,7 @@
                 </div>
                 <!-- 分类列表 -->
                 <div v-for="(v,i) in tab.classifyList" :class="['tab-pane',indexTab==i+1?'active':'']">
-                    <ArticleList v-for="(x,y) in v.lists" :Data="{'data':x,'index':y}" :listStyle="v.type" />
+                    <ArticleList v-for="(x,y) in v.lists" :Data="{'data':x,'index':y}" :listStyle="v.styleType" />
                     <div class="theme-pagination">
                         <div class="next-page ajax-next">
                             <a href="">
@@ -74,6 +74,7 @@ let tab={
     latestList:{
         name:'最新发布',
         href:'http://www.baidu.com',
+        styleType:'list',
         lists:[
             {
                 title:'优雅的支付系统-给站长提供强劲的生产力',
@@ -252,7 +253,7 @@ let tab={
         {
             name:'科技新闻',
             href:'http://www.baidu.com',
-            type:'list',
+            styleType:'list',
             lists:[
                 {
                     title:'超级耐看的小可爱美女图集',
@@ -430,8 +431,51 @@ let tab={
         {
             name:'健康养生',
             href:'http://www.baidu.com',
-            type:'card',
-            lists:[  
+            styleType:'card',
+            lists:[
+                {
+                    title:'优雅的支付系统-给站长提供强劲的生产力',
+                    sub:'支付功能简介及体验',
+                    type:'article',
+                    covers: ['src/assets/image/article/f19c089b3d284d7e9bd54eb39ab1b972.jpeg'],
+                    href:'http://www.baidu.com',
+                    intro:'子比主题本次更新，终于带来了大家期待的支付功能，这下方便站长愉快的收钱啦.... 作为一款资讯阅读类的优质主题，付费阅读、付费下载、付费VIP等功能是必不可少的，子比主题本次更新则带来了付...',
+                    istop: true,
+                    tags:[
+                        {
+                            name:'付费阅读',
+                            pay:{
+                                sum:0.3
+                            },
+                            bgColor: 'jb-yellow'
+                        },
+                        {
+                            name:'智能硬件',
+                            icon:'icon-folder',
+                            bgColor: 'c-yellow'
+                        },
+                        {
+                            name:'科技新闻',
+                            icon:'icon-folder',
+                            bgColor: 'c-green'
+                        },
+                        {
+                            name:'# 付费下载'
+                        },
+                        {
+                            name:'# 支付功能'
+                        }
+                    ],
+                    author:{
+                        id:1,
+                        name:'糖巴',
+                        img:'src/assets/image/article/bizh-4-1.jpeg'
+                    },
+                    time:'2年前',//2021-04-09 23:38:43
+                    comment:7,
+                    views: '3.2W+',
+                    like:434
+                }, 
                 {
                     title:'iPhone XS：合格的 iPhone X 升级机型',
                     sub:'图片类型文章-幻灯片封面体验2',
@@ -475,148 +519,136 @@ let tab={
                     views: '3.2W+',
                     like:434
                 },
-                // {
-                //     title:'超级耐看的小可爱美女图集',
-                //     sub:'图库文章及图片灯箱体验',
-                //     type:'pic',
-                //     covers:{
-                //         num:44,
-                //         lists:[
-                //         'src/assets/image/article/21b8cafa3bf84aeea60fbc47a2caf8f0.jpeg',
-                //         'src/assets/image/article/21b8cafa3bf84aeea60fbc47a2caf8f0.jpeg',
-                //         'src/assets/image/article/6ab766676b6cbeda03493791e253f6ab.jpeg',
-                //         'src/assets/image/article/ce78171be8151585a9e868f2258988f1.jpeg'
-                //         ]
-                //     },
-                //     href:'http://www.baidu.com',
-                //     intro:'子比主题本次更新，终于带来了大家期待的支付功能，这下方便站长愉快的收钱啦.... 作为一款资讯阅读类的优质主题，付费阅读、付费下载、付费VIP等功能是必不可少的，子比主题本次更新则带来了付...',
-                //     istop: false,
-                //     tags:[
-                //         {
-                //             name:'智能硬件',
-                //             icon:'icon-folder',
-                //             bgColor: 'c-yellow'
-                //         },
-                //         {
-                //             name:'轻松时刻',
-                //             icon:'icon-folder',
-                //             bgColor: 'c-green'
-                //         },
-                //         {
-                //             name:'# 博客主题'
-                //         }
-                //     ],
-                //     author:{
-                //         id:1,
-                //         name:'糖巴',
-                //         img:'src/assets/image/article/bizh-4-1.jpeg'
-                //     },
-                //     time:'2年前',//2021-04-09 23:38:43
-                //     comment:1,
-                //     views: '2W+',
-                //     like:428
-                // },
-                // {
-                //     title:'GoPro 宣布收购 ReelSteady',
-                //     sub:'积分商品体验',
-                //     type:'article',
-                //     covers:['src/assets/image/article/20200903@陆家嘴金融贸易区-中国上海.jpg'],
-                //     video:'http://127.0.0.1:5173/src/assets/video/test.mp4',//https://vip.lz-cdn.com/20220606/17379_0df2db27/index.m3u8
-                //     href:'http://www.baidu.com',
-                //     intro:'经常接触 GoPro 运动相机的人应该都听说过 ReelSteady，这是一款面向 After Effects 的增稳插件，可以在后期为视频实现更为稳定的效果，此外还有一款于去年推出的 ReelSte......',
-                //     istop: true,
-                //     tags:[
-                //         {
-                //             name:'付费阅读',
-                //             pay:{
-                //                 sum:0.3
-                //             },
-                //             bgColor: 'jb-yellow'
-                //         },
-                //         {
-                //             name:'智能硬件',
-                //             icon:'icon-folder',
-                //             bgColor: 'c-yellow'
-                //         },
-                //         {
-                //             name:'科技新闻',
-                //             icon:'icon-folder',
-                //             bgColor: 'c-green'
-                //         },
-                //         {
-                //             name:'# 付费下载'
-                //         },
-                //         {
-                //             name:'# 支付功能'
-                //         }
-                //     ],
-                //     author:{
-                //         id:1,
-                //         name:'糖巴',
-                //         img:'src/assets/image/article/bizh-4-1.jpeg'
-                //     },
-                //     time:'2年前',//2021-04-09 23:38:43
-                //     comment:7,
-                //     views: '3.2W+',
-                //     like:434
-                // },
-                // {
-                //     title:'子比主题优雅的支付体验',
-                //     sub:'付费商品-付费图库体验',
-                //     type:'article',
-                //     covers:['src/assets/image/article/f291c740628cd8a58ca2f0efe443a018.jpeg','src/assets/image/article/0af7334cd892a851701ba4aa232e5dc6.jpeg','src/assets/image/article/200580fcde9031843c0d03494e58ea65.jpeg'],
-                //     href:'http://www.baidu.com',
-                //     intro:'子比主题V5.2版本新增付费图片、付费视频功能，此文章为付费图片功能体验！ 支持设置免费查看数量 其它付费功能体验：',
-                //     istop: false,
-                //     tags:[
-                //         {
-                //             name:'付费阅读',
-                //             pay:{
-                //                 sum:0.3
-                //             },
-                //             bgColor: 'jb-yellow'
-                //         },
-                //         {
-                //             name:'智能硬件',
-                //             icon:'icon-folder',
-                //             bgColor: 'c-yellow'
-                //         },
-                //         {
-                //             name:'科技新闻',
-                //             icon:'icon-folder',
-                //             bgColor: 'c-green'
-                //         },
-                //         {
-                //             name:'# 付费下载'
-                //         },
-                //         {
-                //             name:'# 支付功能'
-                //         }
-                //     ],
-                //     author:{
-                //         id:1,
-                //         name:'糖巴',
-                //         img:'src/assets/image/article/bizh-4-1.jpeg'
-                //     },
-                //     time:'2年前',//2021-04-09 23:38:43
-                //     comment:7,
-                //     views: '3.2W+',
-                //     like:434
-                // }
+                {
+                    title:'超级耐看的小可爱美女图集',
+                    sub:'图库文章及图片灯箱体验',
+                    type:'pic',
+                    covers:{
+                        num:44,
+                        lists:[
+                        'src/assets/image/article/21b8cafa3bf84aeea60fbc47a2caf8f0.jpeg',
+                        'src/assets/image/article/21b8cafa3bf84aeea60fbc47a2caf8f0.jpeg',
+                        'src/assets/image/article/6ab766676b6cbeda03493791e253f6ab.jpeg',
+                        'src/assets/image/article/ce78171be8151585a9e868f2258988f1.jpeg'
+                        ]
+                    },
+                    href:'http://www.baidu.com',
+                    intro:'子比主题本次更新，终于带来了大家期待的支付功能，这下方便站长愉快的收钱啦.... 作为一款资讯阅读类的优质主题，付费阅读、付费下载、付费VIP等功能是必不可少的，子比主题本次更新则带来了付...',
+                    istop: false,
+                    tags:[
+                        {
+                            name:'智能硬件',
+                            icon:'icon-folder',
+                            bgColor: 'c-yellow'
+                        },
+                        {
+                            name:'轻松时刻',
+                            icon:'icon-folder',
+                            bgColor: 'c-green'
+                        },
+                        {
+                            name:'# 博客主题'
+                        }
+                    ],
+                    author:{
+                        id:1,
+                        name:'糖巴',
+                        img:'src/assets/image/article/bizh-4-1.jpeg'
+                    },
+                    time:'2年前',//2021-04-09 23:38:43
+                    comment:1,
+                    views: '2W+',
+                    like:428
+                },
+                {
+                    title:'GoPro 宣布收购 ReelSteady',
+                    sub:'积分商品体验',
+                    type:'article',
+                    covers:['src/assets/image/article/20200903@陆家嘴金融贸易区-中国上海.jpg'],
+                    video:'http://127.0.0.1:5173/src/assets/video/test.mp4',//https://vip.lz-cdn.com/20220606/17379_0df2db27/index.m3u8
+                    href:'http://www.baidu.com',
+                    intro:'经常接触 GoPro 运动相机的人应该都听说过 ReelSteady，这是一款面向 After Effects 的增稳插件，可以在后期为视频实现更为稳定的效果，此外还有一款于去年推出的 ReelSte......',
+                    istop: true,
+                    tags:[
+                        {
+                            name:'付费阅读',
+                            pay:{
+                                sum:0.3
+                            },
+                            bgColor: 'jb-yellow'
+                        },
+                        {
+                            name:'智能硬件',
+                            icon:'icon-folder',
+                            bgColor: 'c-yellow'
+                        },
+                        {
+                            name:'科技新闻',
+                            icon:'icon-folder',
+                            bgColor: 'c-green'
+                        },
+                        {
+                            name:'# 付费下载'
+                        },
+                        {
+                            name:'# 支付功能'
+                        }
+                    ],
+                    author:{
+                        id:1,
+                        name:'糖巴',
+                        img:'src/assets/image/article/bizh-4-1.jpeg'
+                    },
+                    time:'2年前',//2021-04-09 23:38:43
+                    comment:7,
+                    views: '3.2W+',
+                    like:434
+                },
+                {
+                    title:'子比主题优雅的支付体验',
+                    sub:'付费商品-付费图库体验',
+                    type:'article',
+                    covers:['src/assets/image/article/f291c740628cd8a58ca2f0efe443a018.jpeg','src/assets/image/article/0af7334cd892a851701ba4aa232e5dc6.jpeg','src/assets/image/article/200580fcde9031843c0d03494e58ea65.jpeg'],
+                    href:'http://www.baidu.com',
+                    intro:'子比主题V5.2版本新增付费图片、付费视频功能，此文章为付费图片功能体验！ 支持设置免费查看数量 其它付费功能体验：',
+                    istop: false,
+                    tags:[
+                        {
+                            name:'付费阅读',
+                            pay:{
+                                sum:0.3
+                            },
+                            bgColor: 'jb-yellow'
+                        },
+                        {
+                            name:'智能硬件',
+                            icon:'icon-folder',
+                            bgColor: 'c-yellow'
+                        },
+                        {
+                            name:'科技新闻',
+                            icon:'icon-folder',
+                            bgColor: 'c-green'
+                        },
+                        {
+                            name:'# 付费下载'
+                        },
+                        {
+                            name:'# 支付功能'
+                        }
+                    ],
+                    author:{
+                        id:1,
+                        name:'糖巴',
+                        img:'src/assets/image/article/bizh-4-1.jpeg'
+                    },
+                    time:'2年前',//2021-04-09 23:38:43
+                    comment:7,
+                    views: '3.2W+',
+                    like:434
+                }
             ]
-        },
-        // {
-        //     name:'智能硬件',
-        //     href:'http://www.baidu.com'
-        // },
-        // {
-        //     name:'轻松时刻',
-        //     href:'http://www.baidu.com'
-        // },
-        // {
-        //     name:'SEO优化',
-        //     href:'http://www.baidu.com'
-        // }
+        }
     ]
 }
 const changeTab=(index)=>{
@@ -672,47 +704,7 @@ const changeTab=(index)=>{
                 }
             }
             .posts-item{
-                display: flex;
-                .item-thumbnail{
-                    img{
-                        border-radius: var(--main-radius);
-                    }
-                    .img-badge {
-                        left: 0;
-                        border-radius: 0 50px 50px 0;
-                        right: auto;
-                    }
-                }
-                .item-body{
-                    display: flex;
-                    flex: auto;
-                    flex-direction: column;
-                    justify-content: space-between;
-                    overflow: hidden;
-                    .item-tags{
-                        margin-bottom: 6px;
-                        a {
-                            font-size: 11px;
-                            padding: 2px 5px;
-                            margin-right: 5px;
-                            span{
-                                font-size: .9em;
-                                margin-left: 3px;
-                            }
-                            .iconfont {
-                                font-size: 1em;
-                            }
-                        }
-                    }
-                    .item-meta{
-                        display: flex;
-                        justify-content: space-between;
-                        align-items: center;
-                        .avatar-mini {
-                            transform: translateY(-1px);
-                        }
-                    }
-                }
+                
             }
             .theme-pagination{
                 text-align: center;
@@ -723,27 +715,7 @@ const changeTab=(index)=>{
         }
     }
 }
-// 卡片样式列表
-.posts-item{
-    &.card {
-        width: calc(33.333% - 16px);
-        display: inline-block !important;
-        margin: 8px;
-        padding: 10px;
-        vertical-align: top;
-        transition: .3s;
-        &.style3 {
-            padding: 0;
-            .item-thumbnail, .item-thumbnail img {
-                border-radius: var(--main-radius) var(--main-radius) 0 0;
-            }
-        }
-        .item-thumbnail {
-            width: 100%;
-            padding-bottom: var(--posts-card-scale);
-        }
-    }
-}
+
 
 
 
