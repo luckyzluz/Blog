99ce17921bfb49a35ee31e7737c5ca2e22d64413
luckyzluz <2567046155@qq.com>
2023-04-05 17:53:00 +0800

    New Cover Display Module

---
+++ frontend/frontpage/src/components/graphicCover.vue
@@ -0,0 +1,54 @@
+<template>
+    <!-- 小工具图形封面 :sm="2"-->
+    <div class="container widget-graphic-cover mb10">
+        <el-row :gutter="10" style="--font-size:18px;">
+            <el-col v-for="(v,i) in data"  :span="4">
+                <a :href="v.href">
+                    <div class="graphic hover-zoom-img noshadow" style="padding-bottom: 35%!important;">
+                        <img class="fit-cover lazyloaded" :src="v.img" alt="">
+                        <div class="absolute graphic-mask" style="opacity: 0.33;"></div>
+                        <div class="abs-center text-center graphic-text this-font">{{ v.title }}</div>
+                    </div>
+                </a>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+<script setup>
+import {ref,reactive} from 'vue';
+let data=[
+    {
+        title:'专业测评',
+        href:'http://www.baidu.com',
+        img: 'src/assets/image/article/img201810291457580.jpg'
+    },
+    {
+        title:'开箱体验',
+        href:'http://www.baidu.com',
+        img: 'src/assets/image/article/img201804121526540.jpg'
+    },
+    {
+        title:'用户体验',
+        href:'http://www.baidu.com',
+        img: 'src/assets/image/article/img202003162053320.jpg'
+    },
+    {
+        title:'科技早报',
+        href:'http://www.baidu.com',
+        img: 'src/assets/image/article/bj2.jpg'
+    },
+    {
+        title:'社区热门',
+        href:'http://www.baidu.com',
+        img: 'src/assets/image/article/img201804121517540.jpg'
+    },
+    {
+        title:'玩机技巧',
+        href:'http://www.baidu.com',
+        img: 'src/assets/image/article/102932ayqhuc0o206gftzn-1-scaled.jpg'
+    },
+]
+</script>
+<style lang="scss">
+
+</style>
---
+++ frontend/frontpage/src/components/graphicCover2.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container widget-graphic-cover mb20 hidden-xs-only" style="--font-size:22px;">
+        <div class="graphic hover-zoom-img noshadow mb10" style="padding-bottom: 16%!important;">
+            <video autoplay loop muted class="fit-cover lazyloading" src="https://oss.zibll.com/zibll.com/2023/04/蓝色矩形循环跳动.mp4"></video>
+            <div class="abs-center text-center graphic-text this-font">
+                <div style="margin-bottom: 10px; font-weight: 700; font-size: 1.4em;">{{ data.title }}</div>{{ data.intro }}
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+let data={
+    title:'测试文章',
+    intro:'更优雅的网站主题'
+}
+</script>
