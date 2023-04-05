052df646810d265a239e587c604123432ac6e4c7
luckyzluz <2567046155@qq.com>
2023-04-05 17:53:53 +0800

    New Sidebar Advertising Module

---
+++ frontend/frontpage/src/components/aside/mediaImage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="lz-widget widget_media_image">
+        <a :href="data.href">
+            <img class="" :src="data.img" style="max-width: 100%;height: auto;" alt="">
+        </a>
+    </div>
+</template>
+<script setup>
+let data={
+    img:'src/assets/image/article/463X224.jpg',
+    href:'http://ww.baidu.com'
+}
+</script>
+<style lang="scss">
+.lz-widget.widget_media_image {
+    padding: 0;
+    overflow: hidden;
+    img {
+        width: 100%;
+    }
+}
+</style>
