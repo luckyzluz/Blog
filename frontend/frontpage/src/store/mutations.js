dd6a68f60ae2764bf9d0222e76346e79b166aa13
luckyzluz <2567046155@qq.com>
2023-04-23 17:20:28 +0800

    Add loading page

--- frontend/frontpage/src/store/mutations.js
+++ frontend/frontpage/src/store/mutations.js
@@ -2,5 +2,8 @@
 export default{
     isShowModalBackdrop(state){ //切换模态框显隐
         state.isShowModalBackdrop = !state.isShowModalBackdrop;
+    },
+    changeLoading(state){ // 切换loading页面显隐
+        state.isLoading=!state.isLoading;
     }
 }
